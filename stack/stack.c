0afef6ce127d8e05443733d2797f79424da6b7ea
lipo <cms6662586@gmail.com>
2025-11-12 15:19:34 +0800

    改了你要求的新版本

--- stack/stack.c
+++ stack/stack.c
@@ -4,7 +4,7 @@
 #include "stack.h"
 
 int init(stack_t *stackptr,int size) {
-    if(stackptr == NULL) return -1;
+    if(stackptr == NULL) return STACK_FAIL;
     if(size > 0) {
         stackptr -> elements =(void**) malloc(size*sizeof(void*));//記得malloc的型態是void 不是int
 
@@ -13,41 +13,41 @@ int init(stack_t *stackptr,int size) {
         stackptr -> sp = -1;
         stackptr -> size = size;
 
-        return 0;
+        return STACK_OK;
     }
-    return -1;
+    return  STACK_FAIL;
 }
 
 int isfull(stack_t *stackptr) {
-    if(stackptr == NULL) return -1;
+    if(stackptr == NULL) return STACK_FAIL;
     return (stackptr -> sp >= stackptr -> size - 1);
 }
 
 int push(stack_t *stackptr,void *val) {
-    if(stackptr == NULL) return -1;
+    if(stackptr == NULL) return STACK_FULL;
     if(!isfull(stackptr)) {
         stackptr -> elements[++(stackptr -> sp)] = val;
-        return 0;
+        return STACK_OK;
     }
-    return -1;
+    return STACK_FAIL;
 }
 
 int isempty(stack_t* stackptr) {
-    if(stackptr == NULL) return -1;
+    if(stackptr == NULL) return STACK_FAIL;
     return(stackptr -> sp < 0);
 }
 
 int pop1(stack_t* stackptr,void** retvalue) {
-    if(stackptr == NULL) return -1;
+    if(stackptr == NULL) return STACK_EMPTY;
     if(!isempty(stackptr)) {
         *retvalue = stackptr -> elements[(stackptr -> sp)--];
-        return 0;
+        return STACK_OK;
     }
-    return -1;
+    return STACK_EMPTY;
 }
 
 void *pop2(stack_t* stackptr,int* status) {
-    if(stackptr == NULL) return -1;
+    if(stackptr == NULL) return NULL;
     if(!isempty(stackptr)) {
         *status = 0;
         return stackptr -> elements[(stackptr -> sp)--];
@@ -61,4 +61,14 @@ int isexist(stack_t* stackptr,void* key,int (*compare)(void* key,void* node)){
         if(compare(key,stackptr -> elements[i])==0) return 0;
     }
     return -1;
+}
+
+void free_stack(stack_t *stackptr) {
+    if(!stackptr) return STACK_FAIL;
+    if(stackptr->elements != NULL) {
+        free(stackptr -> elements);
+        stackptr -> elements = NULL;
+    }
+    stackptr->size = 0;
+    stackptr->sp = -1;
 }
